d6d333d47b9186421aee418fc8244e0167d1bd80
Nikita Andreikin <nandreik@gmu.edu>
2020-09-15 19:26:58 -0400

    init main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# initial attempt at websraping netflix OR utilizing https://reelgood.com/roulette/netflix
+# initial attempt at webscraping netflix OR utilizing https://reelgood.com/roulette/netflix
 
 if __name__ == "__main__":
     print "Start"
