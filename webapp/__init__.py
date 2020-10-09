46d590acd3aff991de03e952378d2f646fca3afa
Nikita Andreikin <nandreik@gmu.edu>
2020-10-08 21:52:53 -0400

    updated chromepath and driver to be used from heroku's config, otherwise driver does not work

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -10,9 +10,15 @@
 """
 directions for deploying to heroku
 
-1: git clone proj in cmd
+1: git clone https://github.com/nandreik/NetflixMoviePicker.git
 2: cd to cloned proj in cmd
 3: heroku create netflixmoviepicker
+3.1: add buildpacks for driver in heroku app settings
+    Headless Google Chrome: https://github.com/heroku/heroku-buildpack-google-chrome
+    Chromedriver: https://github.com/heroku/heroku-buildpack-chromedriver
+3.2: add config vars through cmd
+    heroku config:set GOOGLE_CHROME_BIN=/app/.apt/usr/bin/google_chrome
+    heroku config:set CHROMEDRIVER_PATH=/app/.chromedriver/bin/chromedriver
 4: git push heroku master
 5: heroku ps:scale web=1
 6: heroku open
